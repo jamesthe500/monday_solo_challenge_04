d3b467202bd4d3207227fe6e585fbf554b04e8ce
jamesthe500 <jamesddaniels@gmail.com>
2015-08-05 22:07:06 -0500

    Adds hard mode functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,30 +1,36 @@
+
+var names = [];
+var numOfGroups = 0;
+var heightOf1 = 0;
+var mostGroups = 11;
+var fewestGroups = 2;
+
+$.ajax({
+    url: '/data',
+    success: function(data){
+        names = $.makeArray(data);
+        names.shift();
+        console.log(names);
+    }
+}); //ajax call
+
+
+// array shuffler of my own making
+//
+function superArrayShuffle(array){
+    console.log(array);
+    var numToShuffle = array.length;
+    var shuffled = [];
+    for (var i = numToShuffle; i > 0; i--){
+        var index = Math.floor(Math.random() * i);
+        var target = array[index];
+        shuffled.unshift(target);
+        array.splice(index, 1);
+    }
+    names = shuffled;
+}
+
 $(document).ready(function (){
-    var names = [];
-    var numOfGroups = 0;
-    var heightOf1 = 0;
-
-    $.ajax({
-        url: '/data',
-        success: function(data){
-            names = $.makeArray(data);
-            names.shift();
-            console.log(names);
-        }
-    }); //ajax call
-
-    // sets the number of groups to whatever was clicked.
-    // clears out any names from previous runs
-    // makes all groups hidden
-    // removes the height attribute for the next click of random
-    $('.selector').on('click', function(){
-        $('.selector').removeClass('active');
-        numOfGroups = $(this).data('id');
-        $('.group').children().remove();
-        $('.group').addClass('hide');
-        $('.group').removeAttr("style");
-        $(this).addClass('active');
-    });
-
     // checks to see if a number has been clicked first
     // runs shuffle, writes a temp array
     // while there are people in the thunderdome, drop them into groups in turn
@@ -36,37 +42,38 @@ $(document).ready(function (){
         } else {
             superArrayShuffle(names);
             var thunderDome = names.slice();
+            var k = 0;
             while (thunderDome.length > 0) {
                 for (var j = numOfGroups; j > 0 && thunderDome.length > 0; j--) {
                     var currentGroup = $('.groupUp');
                     var nextGroup = currentGroup.next();
                     currentGroup.removeClass('hide').append('<div class="person"><br>' + thunderDome.pop() + '</div>');
+                    currentGroup.children().last().hide().delay(k * 200).slideDown(200);
                     nextGroup.addClass('groupUp');
                     currentGroup.removeClass('groupUp');
+                    k++;
                 }
                 $('.results').contents().removeClass('groupUp');
                 $('.group').first().addClass('groupUp');
             }
-            heightOf1 = $('.group').first().css('height');
+            heightOf1 = 56 + Math.ceil((names.length/numOfGroups)*50.9);
 
-            $('.group').css("height", heightOf1);
+            $('.group').css("height", heightOf1 + "px");
             console.log("css.('height of 1') " + heightOf1);
         }
+
     });
 
-    // array shuffler of my own making
-    //
-    function superArrayShuffle(array){
-        console.log(array);
-        var numToShuffle = array.length;
-        var shuffled = [];
-        for (var i = numToShuffle; i > 0; i--){
-            var index = Math.floor(Math.random() * i);
-            var target = array[index];
-            shuffled.unshift(target);
-            array.splice(index, 1);
-        }
-        names = shuffled;
-    }
-
+// sets the number of groups to whatever was clicked.
+// clears out any names from previous runs
+// makes all groups hidden
+// removes the height attribute for the next click of random
+    $('.selector').on('click', function(){
+        $('.selector').removeClass('active');
+        numOfGroups = $(this).data('id');
+        $('.group').children().remove();
+        $('.group').addClass('hide');
+        $('.group').removeAttr("style");
+        $(this).addClass('active');
+    });
 }); //doc ready
