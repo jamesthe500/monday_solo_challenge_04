3777608cc4b55204a14abdeeb209c24bd1332141
jamesthe500 <jamesddaniels@gmail.com>
2015-08-06 08:47:58 -0500

    Cleans up code and adds comments

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,24 +1,17 @@
-
 var names = [];
 var numOfGroups = 0;
 var heightOf1 = 0;
-var mostGroups = 11;
-var fewestGroups = 2;
 
 $.ajax({
     url: '/data',
     success: function(data){
         names = $.makeArray(data);
         names.shift();
-        console.log(names);
     }
 }); //ajax call
 
-
 // array shuffler of my own making
-//
 function superArrayShuffle(array){
-    console.log(array);
     var numToShuffle = array.length;
     var shuffled = [];
     for (var i = numToShuffle; i > 0; i--){
@@ -35,7 +28,6 @@ $(document).ready(function (){
     // runs shuffle, writes a temp array
     // while there are people in the thunderdome, drop them into groups in turn
     // after each "row" is written, clear out the groupUp identifier, set the 1st group to it.
-    // after all are written, set the height of all to the height of the first
     $('.randomizer').on('click', function(){
         if (numOfGroups == 0){
             alert("Please select a number of groups.");
@@ -44,6 +36,7 @@ $(document).ready(function (){
             var thunderDome = names.slice();
             var k = 0;
             while (thunderDome.length > 0) {
+                // does a single pass of names to each group per iteration
                 for (var j = numOfGroups; j > 0 && thunderDome.length > 0; j--) {
                     var currentGroup = $('.groupUp');
                     var nextGroup = currentGroup.next();
@@ -53,15 +46,14 @@ $(document).ready(function (){
                     currentGroup.removeClass('groupUp');
                     k++;
                 }
+                // resets in preparation for the next pass of name distribution.
                 $('.results').contents().removeClass('groupUp');
                 $('.group').first().addClass('groupUp');
             }
+            // sets height of groups to the height needed to accommodate the largest group(s) at this font size.
             heightOf1 = 56 + Math.ceil((names.length/numOfGroups)*50.9);
-
             $('.group').css("height", heightOf1 + "px");
-            console.log("css.('height of 1') " + heightOf1);
         }
-
     });
 
 // sets the number of groups to whatever was clicked.
